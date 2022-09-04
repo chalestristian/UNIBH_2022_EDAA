a5d2421d9735626e9f46b9e6dd5eb50af78ad71e
chalestristian <thaleseugenio.3012@aluno.unibh.br>
2022-09-04 13:47:21 -0300

    Atividade 5

--- ListaUm/src/exercicio5.java
+++ ListaUm/src/exercicio5.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class exercicio5 {
+    /*
+     *@author Thales Cristian
+     *@author Neuber Tavares
+     */
         public static void main(String[] args) {
             final Scanner input = new Scanner(System.in);
             int[] unicoNumero = new int[5];
