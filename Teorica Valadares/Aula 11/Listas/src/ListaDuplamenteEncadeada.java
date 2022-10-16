d71780d5246de0335ed4e3ad880b2aa0b8313b8a
Thales Cristian <thales.cristianeugenio@gmail.com>
2022-10-15 21:36:48 -0300

    Criando Lista Duplamente Encadeada

---
+++ Teorica Valadares/Aula 11/Listas/src/ListaDuplamenteEncadeada.java
@@ -0,0 +1,26 @@
+public class ListaDuplamenteEncadeada {
+    NoDuplo primeiro;
+    NoDuplo ultimo;
+    int numeroDeNos;
+
+    public ListaDuplamenteEncadeada() {
+        primeiro = ultimo = null;
+        numeroDeNos = 0;
+    }
+
+    public void inserirFinal(NoDuplo novoNo){
+        novoNo.prox = null;
+        novoNo.ant = null;
+
+        if (primeiro == null){
+            primeiro = novoNo;
+        } else {
+            ultimo.prox = novoNo;
+            novoNo.ant =ultimo;
+        }
+        ultimo = novoNo;
+
+        numeroDeNos++;
+
+    }
+}
