09427bb2f491747ee3b3b1672bc10af77f586572
Thales Cristian <thales.cristianeugenio@gmail.com>
2022-10-16 15:18:22 -0300

    Ajustando Nomes Pastas

--- Pratica Otaviano/ListaDois/src/Utilitarios.java
+++
@@ -1,5 +0,0 @@
-public class Utilitarios {
-    public static int GerarNumeroAleatorio(int numeroMin, int numeroMax) {
-        return (int)(Math.random() * (numeroMax - numeroMin + 1) + numeroMin);
-    }
-}
